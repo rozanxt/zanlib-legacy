5312b8f561be92ad6e86e2e0bfb3c00a2a31cb1d
Rozan I. Rosandi <rozanxt@gmail.com>
2015-09-08 08:10:19 +0200

    Added zan.lib.net package

---
+++ ZanLibrary/src/zan/lib/net/NetworkManager.java
@@ -0,0 +1,98 @@
+package zan.lib.net;
+
+import java.io.IOException;
+import java.net.UnknownHostException;
+
+public class NetworkManager {
+
+	private static ServerThread server;
+	private static SocketThread client;
+
+	public static boolean openServer(int port, int slot) {
+		if (port > 60000) {
+			System.err.println("Error opening server: Port number should be lower than 60000!");
+			return false;
+		}
+		if (isServerRunning()) closeServer();
+		boolean done = false;
+		try {
+			server = new ServerThread(port, slot);
+			done = true;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return done;
+	}
+
+	public static boolean openClient(String address, int port) {
+		if (port > 60000) {
+			System.err.println("Error opening server: Port number should be lower than 60000!");
+			return false;
+		}
+		if (isClientRunning()) closeClient();
+		boolean done = false;
+		try {
+			client = new SocketThread(address, port);
+			if (client.isRunning()) done = true;
+		} catch(UnknownHostException e) {
+			e.printStackTrace();
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+		return done;
+	}
+
+	public static boolean closeServer() {
+		if (!isServerRunning()) return false;
+		return server.closeServer();
+	}
+	public static boolean closeClient() {
+		if (!isClientRunning()) return false;
+		return client.closeSocket();
+	}
+
+	public static void setServerWaiting(boolean sw) {
+		if (!isServerRunning()) return;
+		server.setWaitingClient(sw);
+	}
+
+	public static boolean writeToServer(String msg) {
+		if (!isClientRunning()) return false;
+		client.writeToServer(msg);
+		return true;
+	}
+	public static boolean writeToClient(int cid, String msg) {
+		if (!isServerRunning()) return false;
+		server.writeToClient(cid, msg);
+		return true;
+	}
+	public static boolean writeToAllClients(String msg) {
+		if (!isServerRunning()) return false;
+		server.writeToAllClients(msg);
+		return true;
+	}
+
+	public static String getServerInbox() {
+		if (!isServerRunning()) return null;
+		return server.getInbox();
+	}
+	public static String getClientInbox() {
+		if (!isClientRunning()) return null;
+		return client.getInbox();
+	}
+
+	public static boolean isClientOnline(int cid) {
+		if (!isServerRunning()) return false;
+		return server.isClientOnline(cid);
+	}
+
+	public static boolean isServerRunning() {
+		if (server == null) return false;
+		return server.isRunning();
+	}
+	public static boolean isClientRunning() {
+		if (client == null) return false;
+		return client.isRunning();
+	}
+
+}
